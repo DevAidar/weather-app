8784f2ab1b1ec489bc475dffebbb6e8019a90afc
Lime Peach <limepeach@LimePeach.local>
2020-07-05 21:18:38 -0700

    Updates

---
+++ precache-manifest.4780ac6d20c3bbad681b75b2867ed912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd2eda865a3a47031840cf171301c54",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "f1882e583515678adf2f",
+    "url": "/weather-app/static/css/main.8c0f5409.chunk.css"
+  },
+  {
+    "revision": "6e8abccec87dfeec8641",
+    "url": "/weather-app/static/js/2.ba88c4c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.ba88c4c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1882e583515678adf2f",
+    "url": "/weather-app/static/js/main.423b5219.chunk.js"
+  },
+  {
+    "revision": "74afe40ec314086cb468",
+    "url": "/weather-app/static/js/runtime-main.c71d7dba.js"
+  }
+]);
